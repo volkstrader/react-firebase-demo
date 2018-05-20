1a3b83c9cdc5b76dc447c7c68a20517fc2951fce
Chuck Chau <cchau2@gmail.com>
2018-05-20 07:20:25 +0000

    use react router

--- app/src/App.js
+++ app/src/App.js
@@ -1,65 +1,110 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
 import './App.css';
-import firebase from './firebase';
-import '@firebase/firestore'
+// import firebase from './firebase';
+// import '@firebase/firestore'
 
-class App extends Component {
-  constructor() {
-    super();
-    
-    const db = firebase.firestore();
+// const db = firebase.firestore();
 
+class MainLine extends React.Component {
+  constructor(props) {
+    super(props);
     this.state = {
-      currentItem: '',
-      username: '',
-    }
-
-    this.handleChange = (e) => {
-      this.setState({
-        [e.target.name]: e.target.value
-      })
+      mainLine: '',
     };
 
-    this.handleSubmit = (e) => {
-      e.preventDefault();
-      const itemRef = db.collection('items')
-      const item = {
-        title: this.state.currentItem,
-        user: this.state.username
-      };
-      itemRef.add(item);
-      this.setState({
-        currentItem: '',
-        username: '',
-      });
-    }
+    this.handleClick = (e) => {
+      this.props.checkLine(this.state.mainLine);
+    };
   }
 
   render() {
     return (
-      <div className='app'>
-        <header>
-            <div className='wrapper'>
-              <h1>Fun Food Friends</h1>
-              
-            </div>
-        </header>
-        <div className='container'>
-          <section className='add-item'>
-              <form onSubmit={this.handleSubmit}>
-                <input type="text" name="username" placeholder="What's your name?" onChange={this.handleChange} value={this.state.username} />
-                <input type="text" name="currentItem" placeholder="What are you bringing?" onChange={this.handleChange} value={this.state.currentItem} />
-                <button>Add Item</button>
-              </form>
-          </section>
-          <section className='display-item'>
-            <div className='wrapper'>
-              <ul>
-              </ul>
-            </div>
-          </section>
+      <section id="mainline">
+        <div>What's the main # on your account?</div>
+        <div>
+          <input type="text" placeholder="Sprint Phone #" onChange={e => this.setState({ mainLine: e.target.value })} />
+          <button onClick={this.handleClick}>Continue</button>
         </div>
-      </div>
+      </section>
+    );
+  }
+}
+
+class ActiveLines extends React.Component {
+  render() {
+    console.log(this.props.lines);
+    const activeLines = this.props.lines.map(line => {
+      return <li>{line}</li>
+    });
+
+    return (
+      <section id="activelines">
+        <ul>
+          {activeLines}
+        </ul>
+        <button onClick={this.handleClick}>Continue</button>
+      </section>
+    );
+  }
+}
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      mainLine: '',
+      activeLines: undefined,
+    };
+
+    // this.handleChange = (e) => {
+    //   this.setState({
+    //     [e.target.name]: e.target.value
+    //   })
+    // };
+
+    // this.handleSubmit = (e) => {
+    //   e.preventDefault();
+    //   const itemRef = db.collection('items')
+    //   const item = {
+    //     title: this.state.currentItem,
+    //     user: this.state.username
+    //   };
+    //   itemRef.add(item);
+    //   this.setState({
+    //     currentItem: '',
+    //     username: '',
+    //   });
+    // }
+    this.checkLine = (num) => {
+      console.log('checkLine', num);
+      this.setState({ mainLine: num });
+      const activeLines = ['2134567890', '2134567899'];
+      this.setState({ activeLines });
+      return true;
+    }    
+  }
+
+  render() {
+    return (
+      <Router>
+        <div className='app'>
+          <header>
+            <div className='wrapper'>
+              <h1>Sprint Cash Rewards Registration</h1>
+            </div>
+          </header>
+          <div className='container'>
+            <Route exact path="/" render={() => (
+              (!this.state.activeLines) ? <MainLine checkLine={this.checkLine} /> : <Redirect to="/lines" />
+            )} />
+            <Route exact path="/lines" render={() => (
+              (this.state.activeLines) ? <ActiveLines lines={this.state.activeLines} /> : <Redirect to="/" />
+            )} />
+          </div>
+        </div>
+      </Router>
     );
   }
 }
